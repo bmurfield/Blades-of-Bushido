cd9177f9d2fa386d831606e16eb6b8babf972c40
Marv <bmurfield@gmail.com>
2024-11-26 09:53:07 -0600

    fixed file names and folder structure

--- js/instructions.js
+++ js/instructions.js
@@ -1,14 +1,14 @@
-// instructions.js
+
 document
   .getElementById("return-to-title-btn")
   .addEventListener("click", function () {
-    window.location.href = "title.html"; // Navigate back to the title screen
+    window.location.href = "index.html"; // Navigate back to the title screen
   });
 
 document
   .getElementById("start-game-btn")
   .addEventListener("click", function () {
-    window.location.href = "game.html"; // Navigate back to the title screen
+    window.location.href = "game.html"; // Navigate to the game
   });
 
 const backgroundMusic = document.getElementById("background-music");
